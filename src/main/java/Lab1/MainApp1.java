2aae222ad6c241f737ff527c9bf8b14001a0c2f8
151044 <yhchowai@connect.ust.hk>
2023-09-11 12:03:19 +0800

    Unimprove factorial printing

--- src/main/java/Lab1/MainApp1.java
+++ src/main/java/Lab1/MainApp1.java
@@ -12,15 +12,7 @@ public class MainApp1 {
         System.out.println("Welcome to Scientific Calculator!");
         System.out.println("Program started ...");
         System.out.println(b + " to power " + n + " = " + MyLibrary.power(b, n));
-        // System.out.println(n + "! = " + MyLibrary.factorial(n));
-        QuadFunction<BiFunction<String, String, String>, String, String, String, String> fx
-                = (f, h, i, j) -> f.apply(f.apply(h, i), j);
-        System.out.println(fx.apply((k, l) -> k + l, String.valueOf(n), "! = ", String.valueOf(MyLibrary.factorial(n))));
+        System.out.println(n + "! = " + MyLibrary.factorial(n));
         System.out.println("Program Ended ...");
     }
-}
-
-@FunctionalInterface
-interface QuadFunction<A, B, C, D, R> {
-    R apply(A a, B b, C c, D d);
 }
